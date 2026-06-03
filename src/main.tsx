17826b89d13e66c942a4d0c7253c2cd7d4e1237a
Pedro Millán <millanpedrop@gmail.com>
2026-06-03 11:05:07 +0200

    fix: explicit router import for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "@/app/router";
+import { router } from "@/app/router/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
